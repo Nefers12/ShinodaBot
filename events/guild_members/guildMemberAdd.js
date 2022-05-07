5c459b9cadd7400cf2e8736076cca08da5e31d53
Nefers12 <steoxys42@gmail.com>
2022-05-07 12:12:05 +0200

    Message bienvenue

--- events/guild_members/guildMemberAdd.js
+++ events/guild_members/guildMemberAdd.js
@@ -1,5 +1,9 @@
 const dotenv = require('dotenv'); dotenv.config();
 const { User } = require('../../db/models/index');
+const { Cache } = require('../../index');
+const { Guild } = require('../../db/models/index');
+const guild = require('../../db/models/guild.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'guildMemberAdd',
@@ -7,6 +11,20 @@ module.exports = {
     async execute(client, member) {
 
         const user = await User.findOne({ userId: member.id });
+
+        guild.channels = Cache.get( "channels" );
+        if(!guild){
+            guild = await Guild.findOne({ guildId: member.guild.id });
+            Cache.set( "channels", guild.channels);
+        };
+
+        const welcomeEmbed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setDescription(`Bienvenue à toi <@${member.id}> sur le serveur **${member.guild.name}**\nTu fais parti des ${member.guild.memberCount} personnes présentes sur le serveur !\n\nN'oublie pas de lire les règles !`)
+            .setAuthor({ name : member.user.username,iconURL: member.displayAvatarURL()})
+
+
+            member.guild.channels.cache.get(guild.channels.logs).send({embeds: [welcomeEmbed]});
 
         if(!user) {
 
